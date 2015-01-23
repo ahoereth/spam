724f1b58ba39b62bbdbea6ffcb22fc561ecaeae5
Alexander Höreth <a.hoereth@gmail.com>
2015-01-23 17:59:35 +0200

    Small directive adjustments.

--- src/scripts/directives/add-remove-course.js
+++ src/scripts/directives/add-remove-course.js
@@ -20,16 +20,17 @@
         addCourse: '=',
         removeCourse: '='
       },
+      templateUrl: 'partials/directives/add-remove-course.html',
+      /* @ngInject */
       controller: function($rootScope, $scope) {
         var course = $scope.course;
 
         // check if user is enrolled in this course
-        if (!_.isEmpty($rootScope.user.courses) && _.isUndefined(course.enrolled)) {
+        if (! _.isEmpty($rootScope.user.courses) && _.isUndefined(course.enrolled)) {
           var target = _.find($rootScope.user.courses, {course_id: course.course_id});
           course.enrolled = target ? true : false;
         }
-      },
-      templateUrl: 'partials/directives/addRemoveCourse.html'
+      }
     };
   }
 
--- src/scripts/directives/help-fragment.js
+++ src/scripts/directives/help-fragment.js
@@ -13,19 +13,11 @@
       replace: true,
       scope: true,
       transclude: true,
+      templateUrl: 'partials/directives/help-fragment.html',
       link: function(scope, elemt, attrs) {
         scope.slug  = attrs.slug;
         scope.title = attrs.title;
-      },
-      template:
-        '<div class="panel panel-default">'+
-          '<div class="panel-heading" ng-click="open(slug)">'+
-            '<h2 class="panel-title">{{title}}</h2>'+
-          '</div>'+
-          '<div class="panel-body" ng-show="opened==slug">'+
-            '<ng-transclude></ng-transclude>'+
-          '</div>'+
-        '</div>'
+      }
     };
   }
 }());
