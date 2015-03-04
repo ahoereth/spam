2294466e4d7612bc204ce494949b188d4606ba2b
Alexander Höreth <a.hoereth@gmail.com>
2015-03-04 18:31:11 +0100

    Local setups no longer require URL prefix.

--- src/scripts/app.config.js
+++ src/scripts/app.config.js
@@ -1,6 +1,5 @@
 (function() {
   'use strict';
-  /* global window */
 
   angular
     .module('spam')
@@ -15,13 +14,16 @@
     // intercept http requests for general error handling and loading animation
     $httpProvider.interceptors.push('httpIntercept');
 
+    // Set to false to use local api instead of production api deployed on
+    // IKW servers.
+    var ikw = true;
+
     // Set restangular api base url.
     // If the app instance runs on the /~SPAM path we expect the API to run
     // on the same server (either in production or in development with local
     // dev API server), if the app runs on root / we utilize the remote
     // production API.
-    var api = ((window.location.pathname || '').substr(0, 6) !== '/~SPAM') ?
-      'https://cogsci.uni-osnabrueck.de/~SPAM/api' : '/~SPAM/api';
+    var api = ikw ? 'https://cogsci.uni-osnabrueck.de/~SPAM/api' : '/api';
     RestangularProvider.setBaseUrl(api);
   }
 })();
