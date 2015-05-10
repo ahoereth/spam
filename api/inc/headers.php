15f982ed85e8e010a54b29b63a14fc238412af18
Alexander Höreth <a.hoereth@gmail.com>
2015-05-10 19:08:58 +0200

    Allow CORS.

--- api/inc/headers.php
+++ api/inc/headers.php
@@ -3,21 +3,13 @@
 $app->response->headers->set('Content-Type', 'application/json');
 $app->response->headers->set('charset', 'utf8');
 
-// Handle CORS requests. When performing a CORS request only specific users
-// are allowed to access restricted parts of the API - see User::authorize()
-$remote = $app->request->headers->get('ORIGIN');
-$isremote = $remote &&
-            -1 === strpos($app->request->headers->get('host'), $remote);
-define('REMOTE', $isremote);
-if ($isremote) {
-  $app->response->headers->set('Access-Control-Allow-Origin', $remote);
-  $app->response->headers->set('Access-Control-Allow-Credentials', 'true');
-  $app->response->headers->set(
-    'Access-Control-Allow-Methods',
-    'GET, POST, PUT, DELETE'
-  );
-  $app->response->headers->set(
-    'Access-Control-Allow-Headers',
-    'Accept, X-Requested-With, Authorization, Content-Type'
-  );
-}
+$app->response->headers->set('Access-Control-Allow-Origin', '*');
+$app->response->headers->set('Access-Control-Allow-Credentials', 'true');
+$app->response->headers->set(
+  'Access-Control-Allow-Methods',
+  'GET, POST, PUT, DELETE'
+);
+$app->response->headers->set(
+  'Access-Control-Allow-Headers',
+  'Accept, X-Requested-With, Authorization, Content-Type'
+);
