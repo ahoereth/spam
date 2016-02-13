607cc064fe432421ee0ee38de47a2dab69388ced
Alexander Hoereth <a.hoereth@gmail.com>
2016-02-13 12:16:30 +0100

    Add HTTP Security Headers.

--- api/inc/headers.php
+++ api/inc/headers.php
@@ -3,6 +3,15 @@
 $app->response->headers->set('Content-Type', 'application/json');
 $app->response->headers->set('charset', 'utf8');
 
+// Security headers
+$app->response->headers->set('charset', 'utf8');
+$app->response->headers->set('X-Frame-Options', 'SAMEORIGIN');
+$app->response->headers->set('X-Content-Type-Options', 'nosniff');
+$app->response->headers->set('Content-Security-Policy', "default-src 'self'");
+$app->response->headers->set('Strict-Transport-Security', 'max-age=31536000');
+$app->response->headers->set('X-XSS-Protection', '1; mode=block');
+
+// Dev headers
 if (DEV) {
   $allow = '*';
   $origin = $app->request->headers->get('Origin');
@@ -14,7 +23,7 @@ if (DEV) {
   $app->response->headers->set('Access-Control-Allow-Credentials', 'true');
   $app->response->headers->set(
     'Access-Control-Allow-Methods',
-    'GET, POST, PUT, DELETE', 'OPTIONS'
+    'GET, POST, PUT, DELETE, OPTIONS'
   );
   $app->response->headers->set(
     'Access-Control-Allow-Headers',
