133e2d80b0b0358b38cb2c8226c47e10685e670a
Alexander Höreth <ahoereth@gmail.com>
2016-10-02 22:12:22 +0200

    Preselect restangular lodash deps.

--- src/components/lib/restangular.js
+++ src/components/lib/restangular.js
@@ -1,6 +1,14 @@
-import lodash from 'lodash';
+import {
+  includes, isUndefined, isNull, defaults, isArray, each, extend, find, has,
+  initial, last, reduce, isBoolean, keys, isEmpty, bind, union, values, pick,
+  isNumber, omit, map, every, isObject, isFunction
+} from 'lodash-es';
 import 'restangular';
 
-window._ = lodash;
+window._ = {
+  includes, isUndefined, isNull, defaults, isArray, each, extend, find, has,
+  initial, last, reduce, isBoolean, keys, isEmpty, bind, union, values, pick,
+  isNumber, omit, map, every, isObject, isFunction
+};
 
 export default 'restangular';
