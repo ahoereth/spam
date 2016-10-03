fb4458adceca2e49f2467a76c7915c64623fdc01
Alexander Höreth <ahoereth@gmail.com>
2016-10-03 17:21:37 +0200

    Use babel.

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,14 @@
 import nodeResolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
+import babel from 'rollup-plugin-babel';
 
 
 export default {
   entry: 'src/index.js',
   dest: 'src/bundle.js',
-  sourceMap: false,
+  sourceMap: true,
   plugins: [
+    babel({ exclude: 'node_modules/**' }),
     nodeResolve({ jsnext: true }),
     commonjs()
   ]
